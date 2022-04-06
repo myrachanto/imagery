c863bc692a5345bffecb9b9a29d52b6ac45ede46
myrachanto <myrachanto@gmail.com>
2022-04-06 10:16:28 +0300

    imagery processing

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/myrachanto/settings/imagery
+
+go 1.16
+
+require (
+	github.com/myrachanto/custom-http-error v0.0.0-20220403101408-4f46d4e5a285
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
+)
---
+++ index.go
@@ -0,0 +1,139 @@
+package imagery
+import (
+	"fmt"
+	"image/jpeg"
+	"image/png"
+
+	// "image/webp"
+
+	// "io"
+	"net/http"
+	"os"
+
+	// "image"
+	// "image/color"
+	// "log"
+	// "os"
+	// webpbin "github.com/CapsLock-Studio/go-webpbin"
+	// "path/filepath"
+
+	httperrors "github.com/myrachanto/custom-http-error"
+	"github.com/nfnt/resize"
+)
+
+//Imagery ...
+var (
+	Imageryrepository imageryrepository = imageryrepository{}
+)
+
+type imageryrepository struct{}
+
+//Imagetype ...
+func (i imageryrepository) Imagetype(f, filename string) {
+
+	// maximize CPU usage for maximum performance
+	// runtime.GOMAXPROCS(runtime.NumCPU())
+
+	// open the uploaded file
+	// f := "./img.png"
+	file, err := os.Open(f)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	buff := make([]byte, 512) // why 512 bytes ? see http://golang.org/pkg/net/http/#DetectContentType
+	_, err = file.Read(buff)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	filetype := http.DetectContentType(buff)
+
+	fmt.Println(filetype)
+
+	switch filetype {
+	case "image/jpeg", "image/jpg":
+		ResizeJPG(f, filename)
+
+	case "image/png":
+		ResizePng(f, filename)
+
+	default:
+		fmt.Println("Wrong file format")
+	}
+
+}
+
+//ResizePng ...
+func ResizePng(f, filename string) (*os.File, httperrors.HttpErr) {
+
+	file, err := os.Open(f)
+	if err != nil {
+		return nil, httperrors.NewNotFoundError("error opening the file")
+	}
+	// decode png into image.Image
+	img, err := png.Decode(file)
+	if err != nil {
+		return nil, httperrors.NewNotFoundError("error decoding png")
+	}
+
+	// resize to width 60 using Lanczos resampling
+	// and preserve aspect ratio
+	m := resize.Resize(500, 500, img, resize.Lanczos3)
+	// return out, nil
+	out, err := os.Create(filename)
+	if err != nil {
+		return nil, httperrors.NewNotFoundError("error decoding png")
+	}
+	defer out.Close()
+
+	// write new image to file
+	f1 := png.Encode(out, m)
+	if f1 != nil {
+		return nil, httperrors.NewNotFoundError("error encoding png")
+	}
+	return out, nil
+}
+
+//ResizeJPG ...
+func ResizeJPG(f, filename string) (*os.File, httperrors.HttpErr) {
+	file, err := os.Open(f)
+	if err != nil {
+		return nil, httperrors.NewNotFoundError("error opening the file")
+	}
+	// decode png into image.Image
+	img, err := jpeg.Decode(file)
+	if err != nil {
+		return nil, httperrors.NewNotFoundError("error decoding jpeg")
+	}
+
+	// resize to width 60 using Lanczos resampling
+	// and preserve aspect ratio
+	m := resize.Resize(500, 0, img, resize.Lanczos3)
+	out, err := os.Create(filename)
+	if err != nil {
+		return nil, httperrors.NewNotFoundError("error decoding jpeg")
+	}
+	defer out.Close()
+
+	// write new image to file
+	f1 := jpeg.Encode(out, m, nil)
+	if f1 != nil {
+		return nil, httperrors.NewNotFoundError("error encoding jpeg")
+	}
+	return out, nil
+}
+
+// //ResizeJPG ...
+// func Resizewebp(filename string) (*os.File, *httperrors.HttpError) {
+// 	err := webpbin.NewCWebP().
+// 		Quality(80).
+// 		InputFile(filename).
+// 		OutputFile("image.webp").
+// 		Run()
+// 	return out, nil
+// }
